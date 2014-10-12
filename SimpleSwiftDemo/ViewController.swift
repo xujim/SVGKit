631925393e7af70019334eed30cc62be85ece952
C.W. Betts <computers57@hotmail.com>
2014-10-11 22:57:54 -0600

    Let's just have a simple iOS SVG test app for Swift for now.

---
+++ SimpleSwiftDemo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SimpleSwiftDemo
+//
+//  Created by C.W. Betts on 10/11/14.
+//  Copyright (c) 2014 na. All rights reserved.
+//
+
+import UIKit
+import SVGKit.SVGKit
+
+class ViewController: UIViewController {
+    @IBOutlet var svgkView: SVGKFastImageView?
+    
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
