3130f6c961e357d8270f8cd9e58ef38e8ee3294b
C.W. Betts <computers57@hotmail.com>
2014-10-18 18:36:51 -0600

    Add a Swift demo for OS X.

---
+++ MacSwiftDemo/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  MacSwiftDemo
+//
+//  Created by C.W. Betts on 10/18/14.
+//  Copyright (c) 2014 C.W. Betts. All rights reserved.
+//
+
+import Cocoa
+import SVGKit.SVGKit
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(aNotification: NSNotification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(aNotification: NSNotification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ MacSwiftDemo/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  MacSwiftDemo
+//
+//  Created by C.W. Betts on 10/18/14.
+//  Copyright (c) 2014 C.W. Betts. All rights reserved.
+//
+
+import Cocoa
+import SVGKit.SVGKit
+
+class ViewController: NSViewController {
+
+    @IBOutlet var layeredImage: SVGKLayeredImageView?
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: AnyObject? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
