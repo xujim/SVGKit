8c414b1415c6daba36ded44a1fee0c0d59d0d78e
Vivek Gani <vivek@gani.org>
2014-12-14 18:38:32 -0800

    rename of cocoalumberjack flag_info reference to lumberjack 2.x term

--- Source/SVGKImage.m
+++ Source/SVGKImage.m
@@ -763,7 +763,7 @@ static inline NSString *exceptionInfo(NSException *e)
 	debugStr = [NSString stringWithFormat:@", call stack symbols: %@",e.callStackSymbols];
 #endif
 	
-	return [NSString stringWithFormat:@"Exception name: \"%@\" reason: %@%@", e.name, e.reason, (ddLogLevel & LOG_FLAG_INFO) == LOG_FLAG_INFO ? debugStr : @""];
+	return [NSString stringWithFormat:@"Exception name: \"%@\" reason: %@%@", e.name, e.reason, (ddLogLevel & DDLogFlagInfo) == DDLogFlagInfo ? debugStr : @""];
 }
 
 -(CALayer *)CALayerTree
